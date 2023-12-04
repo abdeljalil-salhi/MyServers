896f83cba6a2223fe1f149fead48ac80952e6326
Abdel <abdelisloading@gmail.com>
2023-12-04 04:38:02 +0100

    Add server model fields

--- server/src/main/java/abdel/codes/myservers/model/Server.java
+++ server/src/main/java/abdel/codes/myservers/model/Server.java
@@ -2,17 +2,44 @@ package abdel.codes.myservers.model;
 
 import abdel.codes.myservers.enum.Status;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.validation.constraints.NotEmpty;
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+
 /**
  * @author Abdeljalil Salhi
  * @version 1.0
  */
 
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Server {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
+    @Column(unique = true)
+    @NotEmpty(message = "IP Adress cannot be empty")
     private String ipAdress;
+    
     private String name;
+    
     private String memory;
+    
     private String type;
+    
     private String image;
+    
     private Status status;
 }
